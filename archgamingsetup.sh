a3e4b1c339539f898bea7edecc8950ac2cb515e8
sneccolol2k <85910739+sneccolol2k@users.noreply.github.com>
2021-06-15 22:03:38 +0000

    Update archgamingsetup.sh

--- archgamingsetup.sh
+++ archgamingsetup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Installs the dependencies to support the Vulkan API (Comment Out Below if AMD)
+# Comment Out Below if AMD)
 # sudo pacman -S lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
 
-# Installs the dependencies to support the Vulkan API (Comment Out Below if Nvidia)
+# (Comment Out Below if Nvidia)
 # sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
 
-# Installs the dependencies to support the Vulkan API (Comment Out Below if Intel)
+# (Comment Out Below if Intel)
 # sudo pacman -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 
-# Installs the dependencies for Lutris
+# Installs the dependencies for Lutris and Winetricks
 sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader winetricks
 
 # Installs Gamemode
